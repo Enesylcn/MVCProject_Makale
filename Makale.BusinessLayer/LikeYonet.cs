615ce5167e0f3bc5099efa5746528e7a3cf0a1cf
Enes Yalçın <enesylcn57@outlook.com>
2022-04-07 15:00:06 +0300

    Add project files.

---
+++ Makale.BusinessLayer/LikeYonet.cs
@@ -0,0 +1,36 @@
+﻿using Makale.DataAccsessLayer;
+using Makale.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makale.BusinessLayer
+{
+    public class LikeYonet
+    {
+        Repository<Liked> repo_like = new Repository<Liked>();
+        BusinessLayerResult<Liked> result = new BusinessLayerResult<Liked>();
+        public int LikeSil(Liked like)
+        {
+            int sonuc = repo_like.Delete(like);
+            return sonuc;
+        }
+        public int LikeInsert(Liked like)
+        {
+            int sonuc = repo_like.Insert(like);
+            return sonuc;
+        }
+        public IQueryable<Liked> ListQueryable()
+        {
+            return repo_like.ListQueryable();
+        }
+
+        public Liked LikeBul(int notid,int userid)
+        {
+            Liked like = repo_like.Find(x => x.Note.ID == notid && x.Kullanici.ID == userid);
+            return like;
+        }
+    }
+}
